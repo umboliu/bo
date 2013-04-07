d0ac0e5f4a5e4ef8825ef2809feee9807ee78a14
Bo Liu <umboliu@caen-vnc02.engin.umich.edu>
2013-04-07 10:43:17 -0400

    multi-threaded proxy EECS489PA2

--- proxy.cpp
+++ proxy.cpp
@@ -1,183 +1,345 @@
-/******************************************
- *  EECS 489 PA 1
+/***************************************************************************
+ *  EECS 489 PA 2
  *  Bo Liu
  *  umboliu@umich.edu
+ *  Xi Han
+ *  hanx@umich.edu
  *
- *  This file creates a client that sends message to server through
- *  the specified port, the end of the message is maked by hitting 
- *  <ENTER> twice or encounters EOF. 
+ *  This file creates a proxy that listens to client through specified port
+ *  and transfer the request/response message between client and server.
  *  
- *  client.cpp
- *  	usage: ./client <host_name> <port>
- *             
- *  If you connect to local machine, use localhost as <host_name>
- *  You can use pipe to redirect the file content to the input, 
- *      e.g., ./client hostname port < data
+ *  proxy.cpp
+ *  	usage: ./proxy <port>
  *
- *****************************************/
+ ***************************************************************************/
 
-#include "header.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 #include <iostream>
 #include <string.h>
+#include <pthread.h>
+
+const int BUFFER_SIZE = 200000 ;
+const int SBUFFER_SIZE = 2000;
 
 using namespace std;
 
-void upper_case_conversion(char* q)
-{	while( *q!='\r' && *(q+1)!='\n' ){
+/*Convert lowercase letter into uppercase letter*/
+void upper_case_conversion(char* q,int length)
+{	int i = 1;
+	int dist;
+	dist = (int) ('a' - 'A');
+	while( i <= length ){
 		if( 'a'<=*q && *q<='z' ){
-		     *q =(char) (*q - 32);
+		     *q =(char) (*q - dist);
 		}
 		q++;
+		i++;
 	}
 }
 
+/*Find the first slash*/
+char* find_slash(char* pslash)
+{	while( *pslash != '\0'){
+		if ( *pslash == '/' ) return pslash;
+		else pslash++;
+	}
+	return pslash;
+}
 
+/*Find colon*/
+char* find_colon(char* pcolon)
+{	while(*pcolon != '\0'){
+		if( *pcolon == ':' )  return pcolon;
+		else pcolon++;	
+	}
+	return pcolon;
+}
 
-int main(int argc, char *argv[])
+/*Work function for each thread*/
+void* work_function(void* arg)
 {
-	if (argc < 2) {
-                fprintf(stderr, "ERROR, no port provided\n");
-                exit(1);
-        }
-
-	int proxy_sock,server_sock,msgsock;
-	struct sockaddr_in proxy;
+	int server_sock,msgsock;
+	int buffer_length = 0,length;
+	int i=0,j=0;
+	bool copying = false;        
+	
+	char client_buffer[BUFFER_SIZE],server_buffer[SBUFFER_SIZE];
+	char str[5][4056];	
+        char request[5][4056];
+        char url[4056],port[4056];
+	char *p,*pslash,*pcolon;
+	
 	struct sockaddr_in server;
 	struct hostent *hp;
-	char* host_name;
-	char client_buffer[BUFFER_SIZE],server_buffer[BUFFER_SIZE];
 	
-	/*Create a socket*/
-	proxy_sock = socket(AF_INET,SOCK_STREAM,0);
-	
-	/*Proxy set up*/
-	proxy.sin_family = AF_INET;
-        proxy.sin_addr.s_addr = INADDR_ANY;
-        proxy.sin_port = htons(atoi(argv[1]));
-	
-	/*Bing socket to proxy*/
-	if(bind(proxy_sock,(struct sockaddr *)&proxy, sizeof proxy) == -1){
-               //error("Unable to bind the specified port");
-               exit(0);
+	char error400[] = "Bad Request\r\n"; 
+	char error501[] = "Not Implemented\r\n";
+	int err400_length = strlen(error400);
+        int err501_length = strlen(error501);
+
+	memset(client_buffer,0,BUFFER_SIZE);
+	memset(url,0,4056);
+        memset(port,0,4056);
+        for (int a=0;a<5;a++){          
+                memset(str[a],0,4056);
+                memset(request[a],0,4056);
         }
 
-	/*Set maximum concurrent clients*/
-        listen(proxy_sock, MAX_CONCURRENT);
+	msgsock = * ((int*)arg);
 
-	//while(true)
-        //{       wait for client requests here
-        msgsock = accept(proxy_sock,(struct sockaddr*) NULL, (socklen_t*) NULL);//last two parameters?
-        read(msgsock,client_buffer,BUFFER_SIZE);
-        //cout<< client_buffer <<endl;
-        
-	char str[4][2048];
-	char url[2048];
-	char* p;
-	bool copying = false;
-	int length,i=0,j=0;
-	int count = 0,count1 =0;	
-	/*Extract from client buffer*/
-	char* q;
+	/*Read clients's request,break when receives "\r\n\r\n" or "\n\n"*/
+	while(true){
+	  read(msgsock,client_buffer + buffer_length,BUFFER_SIZE-buffer_length);
+	  buffer_length = strlen(client_buffer);
+	 
+	  if ((client_buffer[buffer_length-1] == '\n' && client_buffer[buffer_length-2] == '\r' &&
+	       client_buffer[buffer_length-3] == '\n' && client_buffer[buffer_length-4] == '\r')||
+	      (client_buffer[buffer_length-1] == '\n' && client_buffer[buffer_length-2] == '\n')
+	     ){
+	  		break;
+	  }	
+	}	
+	
+	/*Bypass \r,\n, and SPACE and Store the first 5 words of the request message in arrays*/
 	p = client_buffer;
-	q = p;
-	while ( *q != '\0'){
-		count1++;
-		q++;
-	}
-	cout<<"count1 is:"<< count1<<endl;
-	upper_case_conversion(p);
-	while ( *p!='\r' && *(p+1)!='\n' ){
-		count++;
-		if( *p == ' '|| *p == '\n' ){
+	while ( i<=4 && ( *p!='\r' || *(p+1)!='\n'|| *(p+2)!='\r' || *(p+3)!='\n') && (*p!='\n'|| *(p+1)!='\n') ){
+		if( *p == ' ' || *p =='\r' || *p=='\n'){
 		     j=0;
 		     if(copying == true){
 			copying = false;i++;
 		     }
+		     p++;
 		}
 		else{
-		str[i][j] = *p;
-		copying = true;
-		j++;
+	 	     str[i][j] = *p;
+		     copying = true;
+		     j++;
+		     p++;
 		}
-		p++;
 	} 
 	
-	cout<<"count is:"<<count<<endl;
-	cout<<"i is:"<<i<<endl; 
-	cout<<"str[0] is:"<<str[0]<<endl;
-	cout<<"str[1] is:"<<str[1]<<endl;
-	cout<<"str[2] is:"<<str[2]<<endl;
-	cout<<"str[3] is:"<<str[3]<<endl;
-	/*
-	if( strcmp(str[0],"GET")  == 0 ) cout<<"OK1\n";
-	if( strncmp(str[1],"http://",7) == 0 ) cout<<"OK2\n";
-	if( strncmp(str[2],"HTTP/1.0",8)  == 0 ) cout<<"OK3\n";
-	*/
-	for (int a=0;a<4;a++)
-		memset(str[a],0,2048);
-	memset(url,0,2048);
-	if( i==2 ){
-	    length = strlen(str[1]);
-	    if( strcmp(str[0],"GET")==0 && strncmp(str[1],"HTTP://",7)==0 && 
-		str[1][length-1] == '/' && strncmp(str[2],"HTTP/1.0",8)==0 ){   
-	   		strncpy(url,str[1]+7,length-8);
-			cout<<"url is:"<<url<<endl;
-	    }			
+	/*Convert "host:" and "HTTP://" into upper case*/
+        if ( str[1][0] =='/' ){
+	     upper_case_conversion( (char*) str[3],5 );/*HOST:*/	
 	}
-	else if ( i==3 ){
-	     	  length = strlen(str[3]);
-		  if(strcmp(str[0],"GET")==0 && str[1][0]=='/' &&
-	             strncmp(str[2],"HTTP/1.0",8)==0 && strncmp(str[3],"HOST:",5)==0){
-		     strncpy(url,str[3]+5,length-5);
-		     strcpy(url+length,str[1]);
-		     cout<<"url is:"<<url<<endl;	
-		  } 
+	else{
+	     upper_case_conversion ( (char*) str[1],7);/*HTTP:// */
 	}	
-	
-	/*-----------------------Parse and connect to server--------------------*/
+
+	/*Parse the client's request*/
+	/*----------HTTP/0.9 and HTTP/1.1 are changed into HTTP/1.0, other versions are not allowed--------*/
+	if(strcmp(str[2],"HTTP/1.0") != 0){
+		if(strcmp(str[2],"HTTP/0.9")==0 || strcmp(str[2],"HTTP/1.1")==0){
+			strcpy(str[2],"HTTP/1.0");
+		} 
+		else{
+		        write(msgsock,error400,err400_length);
+			close(msgsock);
+			delete (int*) arg;
+			return 0;
+		}
+	}
+
+	/*Parse absolute path and relative path*/
+	if ( strcmp(str[0],"GET")== 0 ){
+    	/*---------------------------ABSOLUTE PATH-------------------------------*/
+	    	if( strncmp(str[1],"HTTP://",7)==0){
+			//cout<<"err is "<<str[2]<<endl;
+			pslash = find_slash(str[1]+7);
+			pcolon = find_colon(str[1]+7);
+			if( *pslash!='\0' ){
+				/*Port number is not specified*/
+				if(*pcolon == '\0' || pcolon > pslash){
+	    				strncpy( url,str[1]+7,(int)(pslash-str[1]-7) );
+	    				length = strlen(str[1]);
+	    				strncpy( request[1],str[1]+7+strlen(url),length-7-strlen(url));
+				}
+				/*Port number is specified*/
+				else {
+					strncpy( url,str[1]+7,(int)(pcolon-str[1]-7) );
+					strncpy( port,str[1]+8+strlen(url),(int)(pslash-str[1]-8-strlen(url)));
+					length = strlen(str[1]);
+					strncpy(request[1],str[1]+8+strlen(url)+strlen(port),length-8-strlen(url)-strlen(port));
+				}
+			}
+			/*Missing '/' in the end causes error 400*/
+			else{
+	    			write(msgsock,error400,err400_length);
+	    			close(msgsock);
+			        delete (int*) arg;
+				return 0;
+			}
+    		}
+
+    	/*---------------------------RELATIVE PATH-------------------------------*/
+    		else if( str[1][0]=='/' ){
+		        /*header field name "host:" and header value are separated by SPACE*/
+			 if( strcmp(str[3],"HOST:")==0 ){
+		  	     pslash = find_slash(str[4]);
+		             pcolon = find_colon(str[4]);
+			     /*Port number is not specified*/
+         		     if( *pcolon == '\0'|| pcolon > pslash){
+                        	 strncpy(url,str[4],(int) (pslash -str[4]));
+                           	 strcpy(request[1],str[1]);
+			     }
+			    /*Port number is specified*/
+			     else{
+				   strncpy(url,str[4],(int) (pcolon-str[4]));
+				   strncpy(port,str[4]+strlen(url)+1,(int) (pslash-str[4]-strlen(url)-1));
+			     	   strcpy(request[1],str[1]);
+				 }			
+			 } 
+			/*header field name "host:" and header value are not separated by SPACE*/ 
+			 else if ( strncmp(str[3],"HOST:",5)== 0 ){			
+				   pslash = find_slash(str[3]);
+				   pcolon = find_colon(str[3]+5);
+				   /*Port number is not specified*/
+				   if( *pcolon == '\0'|| pcolon > pslash){
+                          	       strncpy( url,str[3]+5,(int) (pslash-str[3]-5) );
+                            	       strcpy( request[1],str[1]);
+				   }
+				  /*Port number is specified*/
+				   else{
+					 strncpy(url,str[3]+5,(int) (pcolon-str[3]-5));
+					 strncpy(port,str[3]+5+strlen(url)+1,(int) (pslash-str[3]-5-strlen(url)-1));
+					 strcpy(request[1],str[1]);
+				   }
+			      }
+			/*header filed name is not "host:" in relative path form,return error 400 */
+			      else{
+			   	    write(msgsock,error400,err400_length);
+			   	    close(msgsock);
+			            delete (int*) arg;
+				    return 0;
+			      }
+		       }
+		 /*Missing '/' in relative path, return error400*/   
+		     else{
+		 	   write(msgsock,error400,err400_length);
+			   close(msgsock);
+			   delete (int*) arg;
+			   return 0;
+		     }
+	}
+	/*Method is not "GET"(case-sesitive), return error 501*/
+	else{
+	     write(msgsock,error501,err501_length);
+	     close(msgsock);
+             delete (int*) arg;
+	     return 0;
+        }  
+
+	strcpy(request[3],url);	
+
+	/*-----------------Connect to server and tramsmit request and response between server and client-----------------------*/
 	/*Create a socket to connect to server*/
 	server_sock = socket(AF_INET,SOCK_STREAM,0);
         
 	/*Set up server configuration with which the client connects*/
 	server.sin_family = AF_INET;
-	server.sin_port = htons(80);//need to be changed?
-	//cout<<server.sin_port<<endl;
-	hp = gethostbyname(url);
+	if( strlen(port)!= 0 ){
+		server.sin_port = htons(atoi(port));
+	}
+	else {	
+		server.sin_port = htons(80);
+	}
+	
+	/*Return "Page not found" when URL is not valid */
+	if ( (hp = gethostbyname(url)) == (void*) NULL ){
+		write(msgsock,"Page not found",14);
+		close(msgsock);
+		delete (int*) arg;
+		return 0;
+	}
+	
 	memcpy((char*) &server.sin_addr, (char*) hp->h_addr, hp->h_length);
 	
 	/*Connect to server*/
 	connect(server_sock,(struct sockaddr*) &server,sizeof server);
 
 	/*Write request to server*/
-	length = strlen(url);
-	
-	char request[4][2048];
-	strcpy(request[0],"GET / HTTP/1.0\n");
-	strcpy(request[1],"Host:");	
-	strcpy(request[2],url);
-	strcpy(request[3],"\r\n\r\n");
-	/*
-	cout<<request[0];
-	cout<<request[1];
-	cout<<request[2];
-	cout<<request[3];
-	cout<<request[4];
-	cout<<request[5];	   
-	*/	
-
-	for (int a=0; a<4; a++){
-	     write(server_sock,request[a],strlen(request[a]));
-	}		
-	
-	read(server_sock,server_buffer,2000);
-	cout<<server_buffer<<endl;
-
-	
-     
-	//close(msgsock);
-	
-	
-	
+	strcpy(request[0],"GET ");
+	strcpy(request[2]," HTTP/1.0\r\nHost: ");
+	strcpy(request[4],"\r\n\r\n");
 		
+	for (int a=0; a<5; a++){
+	     write(server_sock,request[a],strlen(request[a]));
+	}	
+	
+	/*Read reponse from server and forward them to client until server finishes transmission*/
+	int done_read,done_write;
+	while(true){
+	      /*Read response from server*/
+	      memset(server_buffer,0,SBUFFER_SIZE);
+	      done_read = read(server_sock,server_buffer,SBUFFER_SIZE);
+              
+	      /*Write response to client*/
+	      if( done_read == 0 ){
+		  /*Close message socket when read is done*/
+		  close(msgsock); break;
+	      }
+	      else{
+	            done_write = write(msgsock,server_buffer,done_read);
+	      }	      
+	}
+	
+	delete (int*) arg;
 	return 0;
 }	
+
+
+int main(int argc, char *argv[])
+{
+        if (argc < 2) {
+                fprintf(stderr, "ERROR, no port provided\n");
+                exit(1);
+        }
+	
+        int proxy_sock;
+        struct sockaddr_in proxy;
+	
+        /*Create a socket*/
+        proxy_sock = socket(AF_INET,SOCK_STREAM,0);
+
+        /*Proxy set up*/
+        proxy.sin_family = AF_INET;
+        proxy.sin_addr.s_addr = INADDR_ANY;
+        proxy.sin_port = htons(atoi(argv[1]));
+
+        /*Bind proxy's socket*/
+        if(bind(proxy_sock,(struct sockaddr *)&proxy, sizeof proxy) == -1){
+		exit(0);
+        }
+
+        /*Set maximum concurrent clients*/
+        listen(proxy_sock,10);
+	signal(SIGPIPE,SIG_IGN);
+
+	/*Accept request from clients*/
+        while(true){
+	      int* msgsock = new int;
+	      *msgsock = accept(proxy_sock,(struct sockaddr*) NULL, (socklen_t*) NULL);
+	      if (*msgsock== -1){
+	     	  cout<<"msgsock is -1"<<endl;
+              }      
+	      else{/*Create a thread for accepted client*/
+		   pthread_t tid;
+		   int err = pthread_create(&tid,NULL,work_function,msgsock );
+		   if(err!=0){
+			cout<<"unable to create thread"<<endl;	
+		   }
+	      }	
+	}
+	return 0;
+}
+
+
